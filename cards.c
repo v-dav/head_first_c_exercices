ddf11b87257f5fc4530b64f93325edee4ba33fda
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-03-11 17:06:38 +0100

    slightly modified the program with conditionals

--- cards.c
+++ cards.c
@@ -8,23 +8,27 @@
 
 int main()
 {
-    char card_name[3];
-    int val = 0;
+	char card_name[3];
+	int val = 0;
 
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
-    if (card_name[0] == 'K')
-        val = 10;
-    else if (card_name[0] == 'Q')
-        val = 10;
-    else if (card_name[0] == 'J')
-        val = 10;
-    else if (card_name[0] == 'A')
-        val = 11;
-    else
-        val = atoi(card_name);
-    printf("The card value is: %d\n", val);
-    return (0);
+	puts("Enter the card_name: ");
+	scanf("%2s", card_name);
+	if (card_name[0] == 'K')
+		val = 10;
+	else if (card_name[0] == 'Q')
+		val = 10;
+	else if (card_name[0] == 'J')
+		val = 10;
+	else if (card_name[0] == 'A')
+		val = 11;
+	else
+		val = atoi(card_name);
+	if ((val > 2) && (val < 7))
+		puts("Count has gone up");
+	else if (val == 10)
+		puts("Count has gone down");
+
+			return (0);
 
 }
 
